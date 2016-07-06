de64c7ac8e75f2c194fe0df95b4e901b99910734
dave-read <davre@microsoft.com>
2016-07-06 00:41:11 -0700

    Add workaround for idle timeout errors in web socket
    Idle timeout in loggregator lib is 30 second.

--- oms_nozzle/main.go
+++ oms_nozzle/main.go
@@ -78,9 +78,9 @@ func main() {
 	if err != nil {
 		panic("Error getting Auth Token" + err.Error())
 	}
-	// TODO: Verify and make configurable.  See https://github.com/cloudfoundry-community/firehose-to-syslog/issues/82
-	consumer.KeepAlive = (25 * time.Second)
 	consumer := consumer.New(dopplerAddress, &tls.Config{InsecureSkipVerify: true}, nil)
+	// TODO: Verify and make configurable.  See https://github.com/cloudfoundry-community/firehose-to-syslog/issues/82
+	consumer.SetIdleTimeout(25 * time.Second)
 	// Create firehose connection
 	msgChan, errorChan := consumer.Firehose(firehoseSubscriptionID, authToken)
 	// async error channel
