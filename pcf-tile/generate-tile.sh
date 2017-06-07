9166d048049febeebc1b1d5d02b0260b8896028f
Liz Zhang <zhangli8810@gmail.com>
2017-06-07 11:21:52 +0800

    Update product name in tile

--- pcf-tile/generate-tile.sh
+++ pcf-tile/generate-tile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 pushd ..
-  rm -f pcf-tile/resources/oms-log-analytics-firehose-nozzle.zip
-  zip -r pcf-tile/resources/oms-log-analytics-firehose-nozzle.zip caching/ firehose/ client/ Godeps/ main.go messages/ omsnozzle/ vendor/ \
+  rm -f pcf-tile/resources/azure-log-analytics-nozzle.zip
+  zip -r pcf-tile/resources/azure-log-analytics-nozzle.zip caching/ firehose/ client/ Godeps/ main.go messages/ omsnozzle/ vendor/ \
     -x vendor/github.com/onsi/\* vendor/golang.org/x/sys/unix/\* vendor/gopkg.in/yaml.v2/\* *_test.go
 popd
 
